7dd30f1b6302c26872810690217a08f8d2f9d91e
Julian Peters <julian.l.peters@gmail.com>
2017-05-17 00:53:33 +0200

    preperation stopwatch and settings

---
+++ app/src/main/java/xyz/julianpeters/timedlists/helpers/Settings.java
@@ -0,0 +1,16 @@
+package xyz.julianpeters.timedlists.helpers;
+
+/**
+ * Created by julian on 17.05.17.
+ */
+
+public class Settings {
+
+    public static int COUNTDOWN = 0;
+    public static int STOPWATCH = 1;
+
+    public static int SHOW_FULL = 0;
+    public static int SHOW_SMALL = 1;
+    public static int SHOW_NONE = 2;
+    public static int SHOW_GROUP = 3;
+}
